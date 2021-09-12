e690628883e131fe741ca0d4239ad60ba1d062a9
Natalia Linkova <linkova1109@yandex.ru>
2021-09-13 09:19:35 +1000

    fixed fetch

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
-function menu() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+function fetchHeader () {
+  let header = document.getElementsByTagName("header")
+  
+  fetch('./header.html')
+  .then(response => response.text())
+  .then(data => {
+      document.getElementById("header").innerHTML += data; 
+  })
+}
+console.log(header);
+function fetchFooter() {
+  let footer = document.getElementsByTagName("footer")
+  
+  fetch('./footer.html')
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById("footer").innerHTML += data;
+      })
+}
+
+
+fetchHeader()
+fetchFooter()
